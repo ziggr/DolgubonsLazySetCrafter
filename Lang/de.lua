3b00fce4f901d8c1f53eb26f1c85f31d3f56a4f0
Zig Zichterman <ziggr@ziggr.com>
2017-05-30 18:32:35 -0700

    release 1

---
+++ Lang/de.lua
@@ -0,0 +1,42 @@
+DolgubonSetCrafter = DolgubonSetCrafter or {}
+
+DolgubonSetCrafter.lang = "en"
+
+DolgubonSetCrafter.localizedStrings = {}
+
+DolgubonSetCrafter.localizedStrings.UIStrings = {}
+DolgubonSetCrafter.localizedStrings.UIStrings.patternHeader       = "Wähle dein Material"
+DolgubonSetCrafter.localizedStrings.UIStrings.comboboxHeader      = "Attributes" -- same
+DolgubonSetCrafter.localizedStrings.UIStrings.comboboxDefault     = "Wähle aus <<1>>" 
+DolgubonSetCrafter.localizedStrings.UIStrings.selectPrompt        = "Bitte wähle aus <<1>>"
+DolgubonSetCrafter.localizedStrings.UIStrings.style               = "Style"
+DolgubonSetCrafter.localizedStrings.UIStrings.level               = "Level"
+DolgubonSetCrafter.localizedStrings.UIStrings.CP                  = "CP"
+DolgubonSetCrafter.localizedStrings.UIStrings.armourTrait         = "Armour Trait"
+DolgubonSetCrafter.localizedStrings.UIStrings.weaponTrait         = "Weapon Trait"
+DolgubonSetCrafter.localizedStrings.UIStrings.quality             = "Quality"
+DolgubonSetCrafter.localizedStrings.UIStrings.gearSet             = "Set"
+DolgubonSetCrafter.localizedStrings.UIStrings.addToQueue          = "Zur Warteschlange hinzufügen"
+DolgubonSetCrafter.localizedStrings.UIStrings.queueHeader         = "Herstellungs Warteschlange"
+DolgubonSetCrafter.localizedStrings.UIStrings.clearQueue          = "Leere Wateschlange"
+DolgubonSetCrafter.localizedStrings.UIStrings.resetToDefault      = "Leere Sektion"
+
+
+
+DolgubonSetCrafter.localizedStrings.weaponNames = 
+{
+    "Axt", "Keule", "Schwert", "Streitaxt", "Streitkolben", "Bidenhänder", "Dolch", "Bogen", "Flammenstab", "Froststab", "Blitzstab", "Heilungsstab", "Schild"
+}
+DolgubonSetCrafter.localizedStrings.pieceNames = 
+{
+    "Torso","Füße","Hände","Kopf","Beine","Schultern","Taille","Hemd"
+}
+DolgubonSetCrafter.localizedStrings.armourTypes = 
+{
+    "Schwere", "Mittlere", "Leichte"
+}
+
+
+DolgubonSetCrafter.localizedStrings.optionStrings = {}
+ZO_CreateStringId("SI_BINDING_NAME_SET_CRAFTER_OPEN", "Öffnen/Schließen des set crafters")
+DolgubonSetCrafterWindowAdd:SetDimensions(230,28)
---
+++ Lang/en.lua
@@ -0,0 +1,43 @@
+DolgubonSetCrafter = DolgubonSetCrafter or {}
+
+DolgubonSetCrafter.lang = "en"
+
+DolgubonSetCrafter.localizedStrings = {}
+
+DolgubonSetCrafter.localizedStrings.UIStrings = {}
+DolgubonSetCrafter.localizedStrings.UIStrings.patternHeader       = "Select Pieces"
+DolgubonSetCrafter.localizedStrings.UIStrings.comboboxHeader      = "Attributes"
+DolgubonSetCrafter.localizedStrings.UIStrings.comboboxDefault     = "Select <<1>>"
+DolgubonSetCrafter.localizedStrings.UIStrings.selectPrompt        = "Please select a <<1>>"
+DolgubonSetCrafter.localizedStrings.UIStrings.style               = "Style"
+DolgubonSetCrafter.localizedStrings.UIStrings.level               = "Level"
+DolgubonSetCrafter.localizedStrings.UIStrings.CP                  = "CP"
+DolgubonSetCrafter.localizedStrings.UIStrings.armourTrait         = "Armour Trait"
+DolgubonSetCrafter.localizedStrings.UIStrings.weaponTrait         = "Weapon Trait"
+DolgubonSetCrafter.localizedStrings.UIStrings.quality             = "Quality"
+DolgubonSetCrafter.localizedStrings.UIStrings.gearSet             = "Set"
+DolgubonSetCrafter.localizedStrings.UIStrings.addToQueue          = "Add to Queue"
+DolgubonSetCrafter.localizedStrings.UIStrings.queueHeader         = "Crafting Queue"
+DolgubonSetCrafter.localizedStrings.UIStrings.clearQueue          = "Clear Queue"
+DolgubonSetCrafter.localizedStrings.UIStrings.resetToDefault      = "Clear Selections"
+
+
+
+DolgubonSetCrafter.localizedStrings.weaponNames = 
+{
+    "Axe", "Mace", "Sword", "Battle Axe", "Maul", "Greatsword", "Dagger", "Bow", "Fire Staff", "Ice Staff", "Lightning Staff", "Restoration Staff", "Shield"
+}
+DolgubonSetCrafter.localizedStrings.pieceNames = 
+{
+    "Chest","Feet","Hands","Head","Legs","Shoulders","Belt","Jerkin"
+}
+DolgubonSetCrafter.localizedStrings.armourTypes = 
+{
+    "Heavy", "Medium", "Light"
+}
+
+
+DolgubonSetCrafter.localizedStrings.optionStrings = {}
+--"Chest","Feet","Hands","Head","Legs","Shoulders","Belt","Jerkin" "Axe", "Mace", "Sword", "Battle Axe", "Maul", "Greatsword", "Dagger", "Bow", "Fire Staff", "Ice Staff", "Lightning Staff", "Restoration Staff", "Shield""Heavy", "Medium", "Light"
+--"Select Peices"  "Attributes"  "Select <<1>>"  "Please select a <<1>>"  "Style"  "Level"  "CP"  "Armour Trait"  "Weapon Trait"  "Quality"  "Set"  "Add to Queue"  "Crafting Queue"  "Clear Queue"  "Clear Selections"
+ZO_CreateStringId("SI_BINDING_NAME_SET_CRAFTER_OPEN", "Open/close the Set Crafter")
